4dae46c8832150b68ddc5a079483f61c1f659c5e
nizhnichenkov <36489945+nizhnichenkov@users.noreply.github.com>
2018-12-20 15:08:53 +0000

    Add files via upload

---
+++ CUnit/maxFunction.h
@@ -0,0 +1,17 @@
+/*
+ * maxFunction.h
+ *
+ */
+
+#ifndef MAXFUNCTION_H_
+#define MAXFUNCTION_H_
+
+/*
+ * Returns the maximum between 2 integers
+ * given as input
+ */
+int maxi(int i1, int i2);
+
+#endif /* MAXFUNCTION_H_ */
+
+
